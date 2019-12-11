0a80c834fdd0ba612c0334c4e9675af93f854a9d
RPF Studio <groupe-esp-fr@outlook.fr>
2019-12-11 21:30:27 +0100

    Update fxmanifest.lua

--- rpf_game/fxmanifest.lua
+++ rpf_game/fxmanifest.lua
@@ -5,4 +5,4 @@ games {"rdr3"}
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
 client_script {'client.lua','config.lua','warmenu.lua','menu.lua'}
-server_script {'server.lua'}
+--server_script {'server.lua'}
